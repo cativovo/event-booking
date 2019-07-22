c2116faff83641165d134fde7609dc9776cd4b7e
Mark Leo Cativo <cativomarkleo@gmail.com>
2019-07-22 16:53:40 +0800

    added graphql

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,60 @@
 const express = require('express');
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
 
 app.use(express.json());
+
+const events = [];
+
+app.use(
+  '/graphql',
+  graphqlHttp({
+    schema: buildSchema(`
+        type Event {
+            _id: ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
+        input EventInput {
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
+        type RootQuery {
+            events: [Event!]!
+        }
+
+        type RootMutation {
+            createEvent(eventInput: EventInput): Event
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+      events: () => events,
+      createEvent: ({ eventInput }) => {
+        const event = {
+          _id: Math.random().toString(),
+          ...eventInput,
+        };
+
+        events.push(event);
+
+        return event;
+      },
+    },
+    graphiql: true,
+  }),
+);
 
 module.exports = app;
