07b6325cb8a7b13b7c501b80f49aa60545683be8
Mark Leo Cativo <cativomarkleo@gmail.com>
2019-07-23 19:03:02 +0800

    added booking to schema

--- backend/src/graphql/schema/index.js
+++ backend/src/graphql/schema/index.js
@@ -1,6 +1,14 @@
 const { buildSchema } = require('graphql');
 
 module.exports = buildSchema(`
+    type Booking {
+        _id: ID!
+        event: Event!
+        user: User!
+        createdAt: String!
+        updatedAt: String!
+    }
+
     type Event {
         _id: ID!
         title: String!
@@ -11,10 +19,10 @@ module.exports = buildSchema(`
     }
 
     type User {
-    _id: ID!
-    email: String!
-    password: String
-    createdEvents: [Event!]
+        _id: ID!
+        email: String!
+        password: String
+        createdEvents: [Event!]
     }
 
     input EventInput {
@@ -25,17 +33,20 @@ module.exports = buildSchema(`
     }
 
     input UserInput {
-    email: String!
-    password: String
+        email: String!
+        password: String
     }
 
     type RootQuery {
         events: [Event!]!
+        bookings: [Booking!]!
     }
 
     type RootMutation {
         createEvent(eventInput: EventInput): Event
         createUser(userInput: UserInput): User
+        bookEvent(eventId: ID!): Booking!
+        cancelBooking(bookingId: ID!): Event!
     }
 
     schema {
